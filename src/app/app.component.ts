103b22a355893e009fd881c90f26d290ea9f1249
Alexander Wiebe <alexanderjwiebe@gmail.com>
2018-04-26 22:02:13 -0600

    have everything working but the ports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SocketService } from './socket.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
+  serverMsg = ['', '', ''];
+
+  private socketSubscription: Subscription;
+
+  constructor(private socket: SocketService) { }
+
+  ngOnInit(): void {
+    this.socket.connect();
+
+    this.socketSubscription = this.socket.messages.subscribe((message: string) => {
+      this.serverMsg.push(message);
+
+      console.log('received message from server: ', message);
+
+      this.serverMsg.shift();
+    });
+  }
+
+  getNextWord(): void {
+    this.socket.send('msg');
+  }
+
+  ngOnDestroy() {
+    this.socketSubscription.unsubscribe();
+  }
+
+
 }
